72b8bf246faefbf97b40b7b0753e0bb9233e8b18
Aaron Tukiainen <81102463+t-CK@users.noreply.github.com>
2023-05-10 09:53:51 +0300

    Created Paddle class and added basic functionality to it, updated .gitignore

--- Static_Mesh.py
+++ Static_Mesh.py
@@ -3,7 +3,19 @@
 PADDLE_WIDTH = 50
 
 class Paddle:
-    pass
+    def __init__(self, player_no :int) -> None:
+        __movement_speed = 10
+        # set the player number and paddle x-position accordingly
+        self.__player_no = player_no
+        if player_no == 1:
+            self.__pos_x = 20   # Leave 20 pixel gap between paddle and window border
+        else:
+            pass # x_position for player 2 will be set to window width - (20 - PADDLE_WIDTH)
+
+        def Update(self, delta_time :float, input_value = 0):
+            # Move the paddle
+            self.__pos_x += self.__movement_speed * input_value * delta_time
+            # Clamp to top/bottom of the play area
 
 class Ball:
     # Ball x and y positions as class variable since there's going to be only one instance of this class
@@ -22,3 +34,10 @@ class Ball:
         # Update balls position using delta time
         self._pos_x += self.velocity_h * delta_time
         self._pos_y = self.velocity_v * delta_time
+        
+        # Clamp the ball into play area
+
+    # Reset the ball to default position
+    def Reset(self):
+        self._pos_x = 0
+        self._pos_y = 0
