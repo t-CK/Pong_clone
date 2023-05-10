ba4cb3f95afaeadc214cf3f7eb550a1c59cead84
Aaron Tukiainen <81102463+t-CK@users.noreply.github.com>
2023-05-10 10:17:37 +0300

    Created Window class and added basic functionality to it

--- Graphics.py
+++ Graphics.py
@@ -1,4 +1,26 @@
-
+from pygame import display
+from pygame import surface
+from Static_Mesh import *
 
 class Window:
-    pass
+    # Class member variables
+    # Class is going to be static, so we can add everything as class variable
+    __screen_width  :int
+    __screen_heigth :int
+    __wnd           :surface.Surface
+
+    def __init__(self) -> None:
+        # Initialize display module
+        display.init
+        # Set the values and create the window
+        self.__screen_width = 1080
+        self.__screen_heigth = 750
+        self.__wnd = display.set_mode(self.__screen_width, self.__screen_heigth)
+
+    # Draw objects and render the window
+    def Render(self, p1 :Paddle, p2 :Paddle, ball :Ball):
+        # Clear the screen with blue
+        self.__wnd.fill(0, 0, 255)
+
+        # Flip buffers
+        display.flip()
